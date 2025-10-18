bf5bd61fda811bef68f43851e8077009e7a947f8
Jalvz1 <javieralvz0427@gmail.com>
2025-10-18 01:24:45 -0500

    Added back my validation miffleware file and test file under a different name to prevent conflicts

--- server/src/routes/volunteers.js
+++ server/src/routes/volunteers.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { validateProfile } = require('../middleware/Validation');
+const { validateProfile } = require('../middleware/VolunteerMiddleware');
 const {
   getProfile,
   createProfile,
